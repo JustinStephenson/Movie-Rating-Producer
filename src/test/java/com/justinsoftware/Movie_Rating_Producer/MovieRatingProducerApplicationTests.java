45041cdcd3dd511838c1e5b2610de454fee0db5e
Justin Stephenson <31748422+JustinStephenson@users.noreply.github.com>
2024-10-07 19:24:55 -0700

    initial setup

---
+++ src/test/java/com/justinsoftware/Movie_Rating_Producer/MovieRatingProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.justinsoftware.Movie_Rating_Producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRatingProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
