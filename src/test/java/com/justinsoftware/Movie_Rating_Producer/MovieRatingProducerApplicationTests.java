a46cbc0009ea3650d3d49b04b65735622ee8a299
Justin Stephenson <31748422+JustinStephenson@users.noreply.github.com>
2024-10-12 23:58:36 -0700

    Add start up test

--- src/test/java/com/justinsoftware/Movie_Rating_Producer/MovieRatingProducerApplicationTests.java
+++ src/test/java/com/justinsoftware/Movie_Rating_Producer/MovieRatingProducerApplicationTests.java
@@ -1,13 +1,22 @@
 package com.justinsoftware.Movie_Rating_Producer;
 
+import com.justinsoftware.Movie_Rating_Producer.controller.MovieRatingController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 @SpringBootTest
 class MovieRatingProducerApplicationTests {
 
+	@Autowired
+	private MovieRatingController movieRatingController;
+
 	@Test
 	void contextLoads() {
+		assertThat(movieRatingController).isNotNull();
 	}
 
 }
