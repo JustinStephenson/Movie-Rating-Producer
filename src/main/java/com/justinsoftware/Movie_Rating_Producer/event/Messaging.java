686b0e432923d5ce066ca5cce5ca1e05047221d2
Justin Stephenson <31748422+JustinStephenson@users.noreply.github.com>
2024-10-26 09:56:25 -0700

    adding logging when sending a message

--- src/main/java/com/justinsoftware/Movie_Rating_Producer/event/Messaging.java
+++ src/main/java/com/justinsoftware/Movie_Rating_Producer/event/Messaging.java
@@ -1,16 +1,19 @@
 package com.justinsoftware.Movie_Rating_Producer.event;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @RequiredArgsConstructor
 @Component
 public class Messaging {
-    
+
     private final KafkaTemplate<String, String> kafkaTemplate;
-    
+
     public void sendMessage(String topic, String message) {
         kafkaTemplate.send(topic, message);
+        log.info("Message sent to topic: {}", topic);
     }
 }
